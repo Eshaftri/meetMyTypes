6e46d295f23f268c945e75f342eb8d5dbffa4215
Nathaniel <leinahtaniel@gmail.com>
2020-01-15 23:37:52 +0800

    Update aws.js

--- meetmytypesadmin/src/js/config/aws.js
+++ meetmytypesadmin/src/js/config/aws.js
@@ -1,5 +1,5 @@
 exports.nkeys = {
     region: "us-east-1",
-    accessKeyId: "AKIAYN2BWYMGBAGZD3OD",
-    secretAccessKey: "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"
+    accessKeyId: "",
+    secretAccessKey: ""
 }
